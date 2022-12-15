cbb7edb8942db93dc320b077a6693db0b6916db3
israelbolvr <112780022+israelbolvr@users.noreply.github.com>
2022-12-15 18:09:54 -0300

    Tela Eliminatoria

---
+++ src/app/controller/TelaMenuEliminatoriasController.java
@@ -0,0 +1,135 @@
+package app.controller;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.layout.VBox;
+
+public class TelaMenuEliminatoriasController {
+
+    @FXML
+    private VBox oitavasGRP1;
+
+    @FXML
+    private VBox oitavasGRP2;
+
+    @FXML
+    private VBox oitavasGRP3;
+
+    @FXML
+    private VBox oitavasGRP4;
+
+    @FXML
+    private VBox oitavasGRP5;
+
+    @FXML
+    private VBox oitavasGRP6;
+
+    @FXML
+    private VBox oitavasGRP7;
+
+    @FXML
+    private VBox oitavasGRP8;
+
+    @FXML
+    private Label oitavasTIME1;
+
+    @FXML
+    private Label oitavasTIME10;
+
+    @FXML
+    private Label oitavasTIME11;
+
+    @FXML
+    private Label oitavasTIME12;
+
+    @FXML
+    private Label oitavasTIME13;
+
+    @FXML
+    private Label oitavasTIME14;
+
+    @FXML
+    private Label oitavasTIME15;
+
+    @FXML
+    private Label oitavasTIME16;
+
+    @FXML
+    private Label oitavasTIME2;
+
+    @FXML
+    private Label oitavasTIME3;
+
+    @FXML
+    private Label oitavasTIME4;
+
+    @FXML
+    private Label oitavasTIME5;
+
+    @FXML
+    private Label oitavasTIME6;
+
+    @FXML
+    private Label oitavasTIME7;
+
+    @FXML
+    private Label oitavasTIME8;
+
+    @FXML
+    private Label oitavasTIME9;
+
+    @FXML
+    private VBox quartasGRP1;
+
+    @FXML
+    private VBox quartasGRP2;
+
+    @FXML
+    private VBox quartasGRP3;
+
+    @FXML
+    private VBox quartasGRP4;
+
+    @FXML
+    private Label quartasTIME1;
+
+    @FXML
+    private Label quartasTIME2;
+
+    @FXML
+    private Label quartasTIME3;
+
+    @FXML
+    private Label quartasTIME4;
+
+    @FXML
+    private Label quartasTIME5;
+
+    @FXML
+    private Label quartasTIME6;
+
+    @FXML
+    private Label quartasTIME7;
+
+    @FXML
+    private Label quartasTIME8;
+
+    @FXML
+    private VBox semiGRP1;
+
+    @FXML
+    private VBox semiGRP2;
+
+    @FXML
+    private Label semiTIME1;
+
+    @FXML
+    private Label semiTIME2;
+
+    @FXML
+    private Label semiTIME3;
+
+    @FXML
+    private Label semiTIME4;
+
+}
